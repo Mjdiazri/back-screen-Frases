5ac9deaaf3567558c86beca796369b4f7457855f
Mjdiazri <citruss2012@hotmail.es>
2024-11-10 09:35:15 -0500

    Initial commit

---
+++ src/main/java/com/pinguiproductions/screen_Frases/ScreenFrasesApplication.java
@@ -0,0 +1,13 @@
+package com.pinguiproductions.screen_Frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenFrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinguiproductions/screen_Frases/ScreenFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinguiproductions.screen_Frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
